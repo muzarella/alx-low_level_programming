ffcd55c0f9df0632fe16de354dedd7de0219b954
muzarella <muzarelladafirst@gmail.com>
2022-09-10 08:41:42 -0700

    random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,17 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *
  *	main - Entry point
- *	Description - Prints if random generated number is +tive, zero or -tive
- *
+ *	Description - Prints Random numbers
  *	Return: Always 0 (Success)
  */
-
 int main(void)
 {
-
 		int n;
+
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 		if (n > 0)
