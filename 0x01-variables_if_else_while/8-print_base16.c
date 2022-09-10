281bf5d1c6e0f69d689080c776cdf88e31649d80
muzarella <muzarelladafirst@gmail.com>
2022-09-10 15:41:31 -0700

    commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *	main - Print base 16 
+ * 	main - Print base
  *	Return: Always 0 (Success)
  */
 int main(void)
@@ -9,7 +9,7 @@ int main(void)
 
 		for (hexa = '0'; hexa <= '9'; hexa++)
 			putchar(hexa);
-		for (hexa = 'a'; hexa <= 'f'; hexa++)
+		Ifor (hexa = 'a'; hexa <= 'f'; hexa++)
 			putchar(hexa);
 		putchar('\n');
 		return (0);
