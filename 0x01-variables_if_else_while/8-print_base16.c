c8b86af2995545197f6b9c2f815028fe1c228e14
muzarella <muzarelladafirst@gmail.com>
2022-09-10 15:34:36 -0700

    commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,10 +6,11 @@
 int main(void)
 {
 		char hexa;
+
 		for (hexa = '0'; hexa <= '9'; hexa++)
-			putchar(hexa)
+			putchar(hexa);
 		for (hexa = 'a'; hexa <= 'f'; hexa++)
 			putchar(hexa);
 		putchar('\n');
-		return (0)
+		return (0);
 }
