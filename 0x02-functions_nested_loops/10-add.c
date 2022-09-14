b2184fbb006e4a27b19008107e9d5faf5f5f817e
muzarella <muzarelladafirst@gmail.com>
2022-09-14 15:54:56 -0700

    files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *	add - Add two integer.
+ *	Return: result
+ *
+ */
+
+int add(int a, int b)
+{
+		int total;
+		total = a + b;
+		return (total);
+}
+
+
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *	print_last_digit - prints the last digit of a number
+ *	@n: the argument to check for the last digit.
+ *	Return: the last digit
+ */
+
+int print_last_digit(int n)
+{
+		int last_digit;
+		
+		if (n < 0)
+			last_digit = (n % 10) * -1;
+		else
+			last_digit = n % 10;
+		_putchar(last_digit + '0');
+		return (last_digit);
+}
