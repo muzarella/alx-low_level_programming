cb7359d72dad76524cf51b8d472c331734d14a36
muzarella <muzarelladafirst@gmail.com>
2022-09-14 11:10:52 -0700

    print

---
+++ 0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *	main - Entry point.
+ *	Return: Always 0 (Success)
+ */
+int main(void){
+
+		char put[] = "_putchar";
+		int i ;
+		
+		for (i = 0; i < 8 ; i++ )
+		{
+			putchar(put[i]);
+		}
+		putchar('\n');
+		return (0);
+}
