b6c1dc51aefaf1d3ada168d5150312c4a38467fa
muzarella <muzarelladafirst@gmail.com>
2022-09-17 05:28:37 -0700

    add

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _mul - multiply two numbers
+ * @b: Input to be multiplied
+ * @a: Input to be multiplied
+ * Return 1 (Sucess) OR 0 (Failure)
+ */
+
+int mul(int a, int b)
+{
+		return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN
-
 #define MAIN
+
 extern int _putchar(char c);
 extern int _isupper(int c);
 extern int _isdigit(int c);
+extern int mul(int a, int b);
 
 #endif
