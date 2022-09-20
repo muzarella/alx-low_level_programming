0438a3d59cc8a13ed3ccaf403fe927b6bd109165
muzarella <muzarelladafirst@gmail.com>
2022-09-20 13:37:54 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - Directs a pointer and changes the value to 98.
+ * @n: The variable pointing to the address.
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
