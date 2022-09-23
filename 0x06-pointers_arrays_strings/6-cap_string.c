40bd863c138d9cedcbe793937ae37c5a035db011
muzarella <muzarelladafirst@gmail.com>
2022-09-23 09:10:48 -0700

    added 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: string.
+ * Return: string.
+ */
+char *cap_string(char *s)
+{
+		int i;
+		
+		i = 0;
+		return (s);
+}
