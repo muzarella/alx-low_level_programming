71d1c625d74361102e56f46cad1c43eca621e099
muzarella <muzarelladafirst@gmail.com>
2022-09-28 22:49:54 -0700

    add

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - main funct.
+ * @n: int n.
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+		if (n ==0)
+			return (0);
+		return (_sqrt(n, 1));
+}
+/**
+ * _sqrt - _sqrt_recursion.
+ * @n: number to multp.
+ * @i: number to multp.
+ * Return: sqrt
+ */
+int _sqrt(int n, int i)
+{
+		if (n < 0)
+			return (-1);
+		if ((i * i) > n)
+			return (-1);
+		if (i * i == n)
+			return (i);
+		return (_sqrt(n, i + 1));
+}
